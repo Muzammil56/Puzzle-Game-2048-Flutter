5ab16a8e8f7c9b5522cd134a1a2837dd81a8e862
unknown <you@example.com>
2021-03-17 00:59:34 +0500

    Puzzle 2048

---
+++ android/app/src/main/kotlin/com/example/puzzle_game_2048/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.puzzle_game_2048
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/tile-colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+final Map<int, Color> tileColors = <int, Color>{
+  2: Colors.teal[50],
+  4: Colors.teal[100],
+  8: Colors.teal[200],
+  16: Colors.teal[300],
+  32: Colors.teal[400],
+  64: Colors.teal[500],
+  128: Colors.teal[600],
+  256: Colors.teal[700],
+  512: Colors.teal[800],
+  1024: Colors.teal[900],
+  2048:Colors.red[900],
+};
